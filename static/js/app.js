d51175ecbc1b7e07362a61a2a5e0711b437a7e20
InfiniteGreen <46692630+InfiniteGreen@users.noreply.github.com>
2019-05-27 01:26:32 -0400

    completed multi-filter with Date + City

--- static/js/app.js
+++ static/js/app.js
@@ -1,19 +1,9 @@
 // from data.js
 var tableData = data;
 
-// Get references to the tbody element, input field and button
-// var $tbody = document.querySelector("tbody");
-// var $dateInput = document.querySelector("#date");
-// var $cityInput = document.querySelector("#city");
-// var $stateInput = document.querySelector("#state");
-// var $countryInput = document.querySelector("#country");
-// var $shapeInput = document.querySelector("#shape");
-// var $searchBtn = document.querySelector("#search");
-// var $resetBtn = document.querySelector("#reset");
-
 // Create empty arrays to store the info
-// var date = [];
-// var city = [];
+var date = [];
+var city = [];
 // var state = [];
 // var country = [];
 // var shape = [];
@@ -30,56 +20,56 @@ submit.on("click", function() {
   d3.event.preventDefault();
 
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+  var date_input = d3.select("#datetime");
+  var city_input = d3.select("#city");
 
-  // Get the value property of the input element  
 
-  if (inputElement.property("value") != ""){
-    date = inputElement.property("value");
+  // Get the value property of the input element - Date
+  if (date_input.property("value") != ""){
+    date = date_input.property("value");
   }
   else {
-    date = inputElement.property("placeholder");
+    date = date_input.property("placeholder");
+  };
+  // Get the value property of the input element - City
+  if (city_input.property("value") != ""){
+    city = city_input.property("value").trim().toLowerCase();
   }
+  else {
+    city = city_input.property("placeholder");
+  };
 
   // console.log(date);
-  //console.log(tableData);
+  // console.log(tableData);
+  // console.log(city);
+
 
   // Create an array with the filtered values
-  var filteredData = tableData.filter(sighting => sighting.datetime === date);
-   
+  // var filteredData = tableData.filter(sighting => sighting.datetime === date);
+  var filteredData = tableData.filter(sighting => {
+    if(sighting.datetime === date){
+        return true;
+    }
+    else if (sighting.city === city){
+      return true;
+    }    
+    // else {
+    //   console.log("No Match Found!");
+    //   retrun false;
+    // }    
+  });
+
 
    console.log(filteredData);
 
-//   // BONUS: Calculate summary statistics for the age field of the filtered data
-
-//   // First, create an array with just the age values
-//   var ages = filteredData.map(person => person.age);
-
 // renderTable renders the filteredData to the tbody
-// function renderTable() {
-//     $tbody.innerHTML = "";
-//     for (var i = 0; i < filteredData.length; i++) {
-//       // Get get the current data object and its fields
-//       var data = filteredData[i];
-//       var fields = Object.keys(data);
-//       // Create a new row in the tbody, set the index to be i + startingIndex
-//       var $row = $tbody.insertRow(i);
-//       for (var j = 0; j < fields.length; j++) {
-//         // For every field in the data object, create a new cell at set its inner text to be the current value at the current address's field
-//         var field = fields[j];
-//         var $cell = $row.insertCell(j);
-//         $cell.innerText = data[field];
-//       }
-//     }
-//   }
-
-// Iterate through each fileteredData and pend to table
 function renderTable() {
 
   var tbody = d3.select("tbody");
           
-  tbody.html("");    
+  tbody.html("");  
 
+  // Iterate through each fileteredData and pend to table
   filteredData.forEach(sighting => {
     
     var tbl_col = Object.keys(sighting);
@@ -96,20 +86,6 @@ function renderTable() {
     });  
   });
 };
-
-// function resetData() {
-//   filteredData = dataSet;
-//   $dateInput.value = "";
-//   $cityInput.value = "";
-//   $stateInput.value = "";
-//   $countryInput.value = "";
-//   $shapeInput.value = "";
-//   renderTable();
-// }
-
-// function resetForm() {
-//   document.getElementById("myForm").reset();
-// }
 
 // // Render the table for the first time on page load
 renderTable();
